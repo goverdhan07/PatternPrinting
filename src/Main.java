f884284fa1daa68e2e45c8b03f52664814bdb0c9
Goverdhan Biradar <goverdhanbiradar@Goverdhans-MacBook-Air.local>
2023-03-22 05:42:37 +0530

    added few patterns

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import java.net.SocketOption;
+
+public class Main {
+    public static void main(String[] args) {
+        pattern1(5);
+        pattern2(5);
+        pattern3(5);
+        pattern4(5);
+        pattern5(10);
+    }
+
+    static void pattern1(int rows){
+        for(int i = 0 ; i < rows; i++ ){
+            for(int j = 0 ; j<rows;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int rows){
+        for(int i = 0 ; i <=rows; i++){
+            for(int j = 0 ; j < i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int rows){
+        for(int i = 0 ; i<=rows; i++){
+            for(int j=0; j<rows-i ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4(int rows){
+        for(int i = 0 ; i <= rows; i++){
+            for(int j = 1 ; j <= i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern5(int rows){
+        for(int i = 0 ; i <= rows;i++){
+            for(int cols = 0 ; cols<=i; cols++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
